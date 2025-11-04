ef32cd5db880a324e471e1a45b658f6dfc2a747a
duckit69 <gherab.radouane@gmail.com>
2025-11-04 22:20:24 +0100

    t

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.status(200).json({message: "Hello World!"})
 })
 
+conn = "mongodb+srv://duckit69:<db_password>@cluster0.azylwdt.mongodb.net/?appName=Cluster0"
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
